1a389f8a64f6fafcf4e1bc5af0ae98919b77e6a3
Anders Hafreager <anderhaf@1x-193-157-210-81.uio.no>
2016-03-14 15:24:12 +0100

    Added more models

---
+++ models/voidmodel.cpp
@@ -0,0 +1,20 @@
+#include "voidmodel.h"
+
+VoidModel::VoidModel()
+{
+
+}
+
+
+bool VoidModel::isInVoid(float x, float y, float z)
+{
+    return true;
+}
+
+void VoidModel::parametersUpdated()
+{
+}
+
+void VoidModel::createParameters()
+{
+}
